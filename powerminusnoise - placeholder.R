67f13324fe074e2f77ed8fec50b702bb9b1596a9
Leon H. <leonhauser@hotmail.com>
2024-09-26 13:24:51 +0200

    Add files via upload
    Placeholder, replace by edit (Leon) of original PNM (Johan)

---
+++ powerminusnoise - placeholder.R
@@ -0,0 +1,13 @@
+# Original code by: Dr. Johan van den Hoogen,  johan.vandenhoogen@usys.ethz.ch
+
+#' @title PMN Calculation Function
+#' @description This function calculates the Power Minus Noise (PMN) index for each minute of a wav file.
+#'
+#' @param files A character vector of file paths to the wav files to be processed.
+#' @out return a data frame containing the PMN values or write to csv.
+#' @param parallel An integer specifying the number of cores to use for parallel processing. If NULL, parallel processing will not be used.
+#'
+#' @return A data frame containing the PMN values for each minute of each wav file.
+
+Placeholder file now,
+Explore if we can share the full code
